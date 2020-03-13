8bffe1ade32313fb408e564a7c734fa83cdedab1
Gean Saturno <gsgoncalves@uolinc.com>
2020-03-13 09:13:20 -0300

    Teste localStorage com 10 tasks

--- tests/unit/libs/TaskListStorage.spec.js
+++ tests/unit/libs/TaskListStorage.spec.js
@@ -50,5 +50,26 @@ describe('TaskListStorage', () => {
       expect(storedTasks).toHaveLength(5);
       expect(storedTasks[4]).toBeInstanceOf(Task);
     });
+
+    it('Com 10 tasks', () => {
+      const taskStorage = new TaskListStorage();
+      const tasks = [
+        new Task('Ir ao dentista'),
+        new Task('Ir ao médico'),
+        new Task('Ir ao mercado'),
+        new Task('Ir ao açougue'),
+        new Task('Lavar as mãos'),
+        new Task('Comprar Cebola'),
+        new Task('Comprar Alho'),
+        new Task('Beber 2L de água'),
+        new Task('Trazer marmita'),
+        new Task('Levar dinheiro'),
+      ];
+
+      taskStorage.setTasks(tasks);
+
+      const storedTasks = taskStorage.getTasks();
+      expect(storedTasks).toHaveLength(10);
+    });
   });
 });

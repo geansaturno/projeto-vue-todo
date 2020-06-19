bece42331b35a551968fa76c3f3a986d4b738545
Gean Saturno <gsgoncalves@uolinc.com>
2020-06-18 21:32:34 -0300

    Remoção de espaços em branco e maiusculas no id da task

--- src/models/Task.model.js
+++ src/models/Task.model.js
@@ -2,6 +2,6 @@ export default class Task {
   constructor(name, done = false) {
     this.name = name;
     this.done = done;
-    this.id = `${name}#${Date.now()}`;
+    this.id = `${name}#${Date.now()}`.toLowerCase().replace(/\s/g, '');
   }
 }
